2de765c3d77228ff8eb03e33efd1b4727039d6d6
rlfrkdms1 <rlfrkdms1@naver.com>
2025-08-18 17:01:17 +0900

    [FEAT]: Spring 프로젝트 세팅

---
+++ src/main/java/com/kt_giga_fms/move_log/MoveLogApplication.java
@@ -0,0 +1,13 @@
+package com.kt_giga_fms.move_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kt_giga_fms/move_log/MoveLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt_giga_fms.move_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
