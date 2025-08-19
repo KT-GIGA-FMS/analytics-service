833cf6094c8efd04a063b711a361d946cd64370b
rlfrkdms1 <83744709+rlfrkdms1@users.noreply.github.com>
2025-08-19 11:05:08 +0900

    [REFACTOR]: 서비스명 변경에 따른 변경

--- src/main/java/com/kt_giga_fms/analytics/AnalyticsApplication.java
+++ src/main/java/com/kt_giga_fms/analytics/AnalyticsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoveLogApplication {
+public class AnalyticsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoveLogApplication.class, args);
