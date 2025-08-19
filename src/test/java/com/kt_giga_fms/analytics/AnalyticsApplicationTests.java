de18bc08ac292d64d6d6cdf84c4ab8b892766125
rlfrkdms1 <83744709+rlfrkdms1@users.noreply.github.com>
2025-08-19 11:04:37 +0900

    [REFACTOR]: 서비스명 변경에 따른 패키지명 변경

--- src/test/java/com/kt_giga_fms/analytics/AnalyticsApplicationTests.java
+++ src/test/java/com/kt_giga_fms/analytics/AnalyticsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MoveLogApplicationTests {
+class AnalyticsApplicationTests {
 
 	@Test
 	void contextLoads() {
